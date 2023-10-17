ee3e3e998087d27688eab873c96649237434a6a4
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-10-18 01:50:05 +0330

    finish Homework1_7

--- Homework1_2.m
+++ Homework1_2.m
@@ -44,3 +44,78 @@ y = conv(x1, h, 'same');
 plot(t1((length(t1) + 1) / 2:end), y, 'LineWidth', 1.5);
 
 legend('Original signal', 'Sampled signal', 'Reconstructed signal')
+%% Homework1_7
+
+t = -5:0.01:5;
+f1 = 4;
+f2 = 5;
+f3 = 10;
+f4 = 20;
+t_1 = -5:1 / f1:5;
+t_2 = -5:1 / f2:5;
+t_3 = -5:1 / f3:5;
+t_4 = -5:1 / f4:5;
+x_t = (sinc (5 * t)) .^ 2;
+x_t1 = (sinc (5 * t_1)) .^ 2;
+x_t2 = (sinc (5 * t_2)) .^ 2;
+x_t3 = (sinc (5 * t_3)) .^ 2;
+x_t4 = (sinc (5 * t_4)) .^ 2;
+figure('Name', 'Original Signal');
+subplot(2, 1, 1)
+plot(t, abs(fftshift(fft(x_t))), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Original signal');
+grid on;
+
+subplot(2, 1, 2)
+plot(t_1, abs(fftshift(fft(x_t1))), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('sampled signal');
+grid on;
+
+figure('Name', 'Original Signal');
+subplot(2, 1, 1)
+plot(t, abs(fftshift(fft(x_t))), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Original signal');
+grid on;
+
+subplot(2, 1, 2)
+plot(t_2, abs(fftshift(fft(x_t2))), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('sampled signal');
+grid on;
+
+figure('Name', 'Original Signal');
+subplot(2, 1, 1)
+plot(t, abs(fftshift(fft(x_t))), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Original signal');
+grid on;
+
+subplot(2, 1, 2)
+plot(t_3, abs(fftshift(fft(x_t3))), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('sampled signal');
+grid on;
+
+figure('Name', 'Original Signal');
+subplot(2, 1, 1)
+plot(t, abs(fftshift(fft(x_t))), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Original signal');
+grid on;
+
+subplot(2, 1, 2)
+plot(t_4, abs(fftshift(fft(x_t4))), 'LineWidth', 2);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('sampled signal');
+grid on;
