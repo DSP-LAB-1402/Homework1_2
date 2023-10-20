7e7c1935fa72121131062ef752db8061e8890916
Mohammad Mahdi Elyasi <lordmahdi79@gmail.com>
2023-10-20 13:13:42 +0330

    Finish Homework1_9

--- Homework1_2.m
+++ Homework1_2.m
@@ -235,3 +235,249 @@ ylabel('Amplitude');
 title('sampled signal');
 grid on;
 
+%% Homework1_9
+
+fs = 2;
+t_max = 256;
+t = -t_max:1 / fs:t_max;
+f1 = 1/16;
+f2 = 5/16;
+f3 = 9/16;
+f4 = 13/16;
+x = cos(2 * pi * f1 * t) + cos(2 * pi * f2 * t) + cos(2 * pi * f3 * t) + cos(2 * pi * f4 * t);
+FT_x = (1 / fs) * fftshift(fft(x));
+f_axis = linspace(-fs / 2, fs / 2, length(FT_x));
+
+figure('Name', 'Fourier Transform of Continuous Time Signal');
+plot(f_axis, abs(FT_x), 'LineWidth', 1.5);
+xlabel('Frequency (Hz)');
+ylabel('Amplitude');
+title('Fourier Transform of Continuous Time Signal');
+xlim([-1, 1]);
+grid on;
+%%%
+% part b
+figure('Name', 'Amplitude and phase 4 analytic filters');
+analy_filter = readmatrix('filters.xls', 'Sheet', 1);
+analy_filter1 = analy_filter(1, :);
+analy_filter2 = analy_filter(2, :);
+analy_filter3 = analy_filter(3, :);
+analy_filter4 = analy_filter(4, :);
+subplot(4, 2, 1)
+w_axis = linspace(0, pi, 1000);
+N = length(analy_filter1);
+n = 0:N -1;
+DTFT_analy_filter1 = analy_filter1 * exp(-1j * n' * w_axis);
+
+plot(w_axis / pi, abs(DTFT_analy_filter1), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Amplitude');
+title('Absolute Value of DTFT analy filter1');
+xlim([0 1]);
+grid on;
+
+subplot(4, 2, 2)
+plot(w_axis / pi, angle(DTFT_analy_filter1), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Phase');
+xlim([0 1]);
+title('Phase of DTFT analy filter1');
+grid on;
+subplot(4, 2, 3)
+w_axis = linspace(0, pi, 1000);
+N = length(analy_filter2);
+n = 0:N -1;
+DTFT_analy_filter2 = analy_filter2 * exp(-1j * n' * w_axis);
+
+plot(w_axis / pi, abs(DTFT_analy_filter2), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Amplitude');
+title('Absolute Value of DTFT analy filter2');
+xlim([0 1]);
+grid on;
+
+subplot(4, 2, 4)
+plot(w_axis / pi, angle(DTFT_analy_filter2), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Phase');
+xlim([0 1]);
+title('Phase of DTFT analy filter2');
+grid on;
+
+subplot(4, 2, 5)
+w_axis = linspace(-0, pi, 1000);
+N = length(analy_filter3);
+n = 0:N -1;
+DTFT_analy_filter3 = analy_filter3 * exp(-1j * n' * w_axis);
+
+plot(w_axis / pi, abs(DTFT_analy_filter3), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Amplitude');
+title('Absolute Value of DTFT analy filter3');
+xlim([0 1]);
+grid on;
+
+subplot(4, 2, 6)
+plot(w_axis / pi, angle(DTFT_analy_filter3), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Phase');
+xlim([0 1]);
+title('Phase of DTFT analy filter3');
+grid on;
+
+subplot(4, 2, 7)
+w_axis = linspace(0, pi, 1000);
+N = length(analy_filter4);
+n = 0:N -1;
+DTFT_analy_filter4 = analy_filter4 * exp(-1j * n' * w_axis);
+
+plot(w_axis / pi, abs(DTFT_analy_filter4), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Amplitude');
+title('Absolute Value of DTFT analy filter4');
+xlim([0 1]);
+grid on;
+
+subplot(4, 2, 8)
+plot(w_axis / pi, angle(DTFT_analy_filter4), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Phase');
+xlim([0 1]);
+title('Phase of DTFT analy filter4');
+grid on;
+
+figure('Name', 'Amplitude and phase 4 Composition filters');
+Composition_filter = readmatrix('filters.xls', 'Sheet', 2);
+Composition_filter1 = Composition_filter(1, :);
+Composition_filter2 = Composition_filter(2, :);
+Composition_filter3 = Composition_filter(3, :);
+Composition_filter4 = Composition_filter(4, :);
+subplot(4, 2, 1)
+w_axis = linspace(0, pi, 1000);
+N = length(Composition_filter1);
+n = 0:N -1;
+DTFT_Composition_filter1 = Composition_filter1 * exp(-1j * n' * w_axis);
+
+plot(w_axis / pi, abs(DTFT_Composition_filter1), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Amplitude');
+title('Absolute Value of DTFT Composition filter1');
+xlim([0 1]);
+grid on;
+
+subplot(4, 2, 2)
+plot(w_axis / pi, angle(DTFT_Composition_filter1), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Phase');
+xlim([0 1]);
+title('Phase of DTFT Composition filter1');
+grid on;
+subplot(4, 2, 3)
+w_axis = linspace(0, pi, 1000);
+N = length(Composition_filter2);
+n = 0:N -1;
+DTFT_Composition_filter2 = Composition_filter2 * exp(-1j * n' * w_axis);
+
+plot(w_axis / pi, abs(DTFT_Composition_filter2), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Amplitude');
+title('Absolute Value of DTFT Composition filter2');
+xlim([0 1]);
+grid on;
+
+subplot(4, 2, 4)
+plot(w_axis / pi, angle(DTFT_Composition_filter2), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Phase');
+xlim([0 1]);
+title('Phase of DTFT Composition filter2');
+grid on;
+
+subplot(4, 2, 5)
+w_axis = linspace(-0, pi, 1000);
+N = length(Composition_filter3);
+n = 0:N -1;
+DTFT_Composition_filter3 = Composition_filter3 * exp(-1j * n' * w_axis);
+
+plot(w_axis / pi, abs(DTFT_Composition_filter3), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Amplitude');
+title('Absolute Value of DTFT Composition filter3');
+xlim([0 1]);
+grid on;
+
+subplot(4, 2, 6)
+plot(w_axis / pi, angle(DTFT_Composition_filter3), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Phase');
+xlim([0 1]);
+title('Phase of DTFT Composition filter3');
+grid on;
+
+subplot(4, 2, 7)
+w_axis = linspace(0, pi, 1000);
+N = length(Composition_filter4);
+n = 0:N -1;
+DTFT_Composition_filter4 = Composition_filter4 * exp(-1j * n' * w_axis);
+
+plot(w_axis / pi, abs(DTFT_Composition_filter4), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Amplitude');
+title('Absolute Value of DTFT Composition filter4');
+xlim([0 1]);
+grid on;
+
+subplot(4, 2, 8)
+plot(w_axis / pi, angle(DTFT_Composition_filter4), 'LineWidth', 1.5);
+xlabel('Frequency (rad/s)');
+ylabel('Phase');
+xlim([0 1]);
+title('Phase of DTFT Composition filter4');
+grid on;
+%%%
+% part c
+analy_filter1_out = filter(analy_filter1, 1, x);
+analy_filter2_out = filter(analy_filter2, 1, x);
+analy_filter3_out = filter(analy_filter3, 1, x);
+analy_filter4_out = filter(analy_filter4, 1, x);
+analy_filter1_out_down = downsample(analy_filter1_out, 4);
+analy_filter2_out_down = downsample(analy_filter2_out, 4);
+analy_filter3_out_down = downsample(analy_filter3_out, 4);
+analy_filter4_out_down = downsample(analy_filter4_out, 4);
+analy_filter1_out_down_pc = 2 * analy_filter1_out_down;
+analy_filter2_out_down_pc = 0 * analy_filter2_out_down;
+analy_filter3_out_down_pc = analy_filter3_out_down;
+analy_filter4_out_down_pc = 0.5 * analy_filter4_out_down;
+analy_filter1_out_up = upsample(analy_filter1_out_down_pc, 4);
+analy_filter2_out_up = upsample(analy_filter2_out_down_pc, 4);
+analy_filter3_out_up = upsample(analy_filter3_out_down_pc, 4);
+analy_filter4_out_up = upsample(analy_filter4_out_down_pc, 4);
+
+Composition_filter1_out = filter(Composition_filter1, 1, analy_filter1_out_up);
+Composition_filter2_out = filter(Composition_filter2, 1, analy_filter2_out_up);
+Composition_filter3_out = filter(Composition_filter3, 1, analy_filter3_out_up);
+Composition_filter4_out = filter(Composition_filter4, 1, analy_filter4_out_up);
+out_of_filter_bank = Composition_filter1_out + Composition_filter2_out + Composition_filter3_out + Composition_filter4_out;
+FT_out_of_filter_bank = (1 / fs) * fftshift(fft(out_of_filter_bank));
+f_axis = linspace(-fs / 2, fs / 2, length(FT_out_of_filter_bank));
+
+figure('Name', 'Fourier Transform of Continuous Time Signal');
+plot(f_axis, abs(FT_out_of_filter_bank), 'LineWidth', 1.5);
+xlabel('Frequency (Hz)');
+ylabel('Amplitude');
+title('Fourier Transform of Continuous Time Signal');
+xlim([-1, 1]);
+
+figure('Name', 'output and input of filter bank');
+f_axis = linspace(-fs / 2, fs / 2, length(FT_x));
+plot(f_axis, abs(FT_x), 'LineWidth', 1.5, 'Color', 'r');
+xlabel('Frequency (Hz)');
+ylabel('Amplitude');
+title('output and input of filter bank');
+xlim([-1, 1]);
+grid on;
+hold on; % Hold the figure
+f_axis = linspace(-fs / 2, fs / 2, length(FT_out_of_filter_bank));
+plot(f_axis, abs(FT_out_of_filter_bank), 'LineWidth', 1.5, 'Color', 'b');
+xlim([-1, 1])
+legend('input', 'output'); % Add legend to the figure
